b021f13426d9e6766292f917ec5eed9c1fa42ac9
chimdi2700 <chimdi2700@gmail.com>
2024-06-05 17:39:48 +0100

    first commit

---
+++ src/portfolio/Footer.js
@@ -0,0 +1,28 @@
+
+
+const Footer = () => {
+  return (
+    <>
+  <footer id="footer">
+    <div className="container">
+      <div className="copyright">
+        © Copyright{" "}
+        <strong>
+          <span>Chimdi</span>
+        </strong>
+      </div>
+      <div className="credits">
+        
+        {/* Licensing information: https://bootstrapmade.com/license/ */}
+        {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ */}
+        Designed by <a href="https://bootstrapmade.com/">Chimdi</a>
+      </div>
+    </div>
+  </footer>
+  {/* End  Footer */}
+</>
+
+  )
+}
+
+export default Footer
